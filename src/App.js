9206473ce1c6e2c3e3be37bea54b39adc9906921
Hooman Pegahmehr <hpegahmehr@gmail.com>
2022-11-18 23:48:02 -0500

    simple card component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortonaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+// console.log(AlexaImage);
+// console.log(SiriImage);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Personal Digital Assistants</div>
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortonaImage} />
+      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
     </div>
   );
 }
-
 export default App;
